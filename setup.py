70c48d8399cb2bc14fdb8cf22141a93ce526e7eb
Spencer McIntyre <zeroSteiner@gmail.com>
2016-03-08 19:22:57 -0500

    Add a simple setup script for installation

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from gw2 import __version__
+
+setup(
+	author='Spencer McIntyre',
+	description='Guild Wars 2 API',
+	name='gw2',
+	packages=['gw2'],
+	url='https://github.com/zeroSteiner/guild-wars-2',
+	version='1.0',
+)
